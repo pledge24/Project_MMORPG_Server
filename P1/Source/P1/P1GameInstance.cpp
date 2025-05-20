f28723741942d5e889515d837165308452446416
pledge24 <104922729+pledge24@users.noreply.github.com>
2025-05-21 01:50:38 +0900

    🎉 add: 언리얼 파일 connect 연결까지 추가

---
+++ P1/Source/P1/P1GameInstance.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "P1GameInstance.h"
+#include "Sockets.h"
+#include "Common/TcpSocketBuilder.h"
+#include "Serialization/ArrayWriter.h"
+#include "SocketSubsystem.h"
+#include "Protocol.pb.h"
+#include "ClientPacketHandler.h"
+
+void UP1GameInstance::ConnectToGameServer()
+{
+	Socket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(TEXT("Stream"), TEXT("Client Socket"));
+
+	FIPv4Address Ip;
+	FIPv4Address::Parse(IpAddress, Ip);
+
+	TSharedRef<FInternetAddr> InternetAddr = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
+	InternetAddr->SetIp(Ip.Value);
+	InternetAddr->SetPort(Port);
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Connecting To Server...")));
+
+	bool Connected = Socket->Connect(*InternetAddr);
+
+	if (Connected)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Connection Success")));
+
+		//// Session
+		//GameServerSession = MakeShared<PacketSession>(Socket);
+		//GameServerSession->Run();
+
+		//// TEMP : Lobby���� ĳ���� ����â ��
+		//{
+		//	Protocol::C_LOGIN Pkt;
+		//	SendBufferRef SendBuffer = ClientPacketHandler::MakeSendBuffer(Pkt);
+		//	SendPacket(SendBuffer);
+		//}
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Connection Failed")));
+	}
+}
+
+void UP1GameInstance::DisconnectFromGameServer()
+{
+	//if (Socket == nullptr || GameServerSession == nullptr)
+	//	return;
+
+	//Protocol::C_LEAVE_GAME LeavePkt;
+	//SEND_PACKET(LeavePkt);
+
+	/*if (Socket)
+	{
+		ISocketSubsystem* SocketSubsystem = ISocketSubsystem::Get();
+		SocketSubsystem->DestroySocket(Socket);
+		Socket = nullptr;
+	}*/
+}
+
---
+++ P1/Source/P1/P1GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "P1GameModeBase.h"
+
